f2ef129f77524e0dba3cb2496ad9d8e2101b7ced
kdurr <kristina.l.durr@gmail.com>
2013-08-24 16:01:59 -0400

    have validation complete for echo_five

---
+++ echo_five.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+puts "What do you want to say?"
+input = gets.chomp
+ 
+responses = []
+ 
+def playback_first(line)
+  puts "You said: "+line
+end
+ 
+def playback_middle(line)
+  puts "Then, you said: "+line
+end
+ 
+def playback_last(line)
+  puts "Finally you said: "+line
+  puts "Phew! Glad you got all #{@count} of these things off your chest!"
+end
+ 
+def validate_file
+  @file_name = gets.chomp
+  if @file_name != "speech.txt"
+    puts "I couldn't find that file."
+    puts "Where can I find what you want to say?"
+    @file_name = gets.chomp
+  end
+end
+
+  if input == "Nothing!"
+    puts "Ok, fine!"
+  elsif input == "I have something prepared"
+    puts "OK, where can I find what you want to say?"
+    validate_file
+
+
+  elsif input == "I have a lot to say"
+      puts "Ok, let's hear it!"
+    while input != 'done'
+    input = gets.chomp
+    responses << input
+    end
+  end
+ 
+  first = responses.first
+  last = responses[-2]
+  responses.pop
+  @count = responses.length
+ 
+responses.each do |x|
+  if x == first
+    playback_first(first.to_s)
+  elsif x == last
+    playback_last(last.to_s)
+  else
+    playback_middle(x)
+  end
+end
